56fd53efcd9ebf5ec7da0dc06ec04a60243200c9
smallDada <872505550@qq.com>
2018-10-22 15:03:18 +0800

    吸顶效果

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,5 +1,4 @@
 <template>
-
   <scroll :data='data'
           class='listview'
           ref='listview'
@@ -37,18 +36,28 @@
         </li>
       </ul>
     </div>
+    <div class="list-fixed"
+         ref="fixed"
+         v-show="fixedTitle">
+      <div class="fixed-title">{{fixedTitle}} </div>
+    </div>
+    <div v-show="!data.length"
+         class="loading-container">
+      <loading></loading>
+    </div>
   </scroll>
 
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
 import {getData} from 'common/js/dom'
 
-// const TITLE_HEIGHT = 30
+const TITLE_HEIGHT = 30
 const ANCHOR_HEIGHT = 18
 export default {
-  components: {Scroll},
+  components: {Scroll, Loading},
   props: {
     data: {
       type: Array,
@@ -56,59 +65,82 @@ export default {
     }
   },
   created() {
+    // better-scroll组件需要
     this.probeType = 3
+    // 右边案例滚动时记录滚动数据
     this.touch = {}
+    // 设置每一个Group的高度位置
     this.listHeight = []
+    // scroll组件传入，实时传回左边滚动的Y值
     this.listenScroll = true
   },
   mounted() {
 
   },
   computed: {
+    // 右边联动的title
     shortcutList() {
       return this.data.map((group, index) => {
         return group.title.substr(0, 1)
       })
+    },
+    // 左边吸顶操作的title
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
     }
   },
   data() {
     return {
-      scrollY: -1,
-      currentIndex: 0
+      scrollY: -1, // 记录左边内容实时的滚动位置
+      currentIndex: 0, // 当前滚动位置对应的Index
+      diff: -1 // 吸顶操作的间隔
     }
   },
   methods: {
+    // 右边触碰点击事件
     onShortcutTouchStart(e) {
-      let anchorIndex = getData(e.target, 'index')
-      let firstTouch = e.touches[0]
-      this.touch.y1 = firstTouch.pageY
-      this.touch.anchorIndex = anchorIndex
+      let anchorIndex = getData(e.target, 'index') // 拿到点击的index
+      let firstTouch = e.touches[0] // 拿到点击的对象
+      this.touch.y1 = firstTouch.pageY // 标记点击对象的位置
+      this.touch.anchorIndex = anchorIndex // 标记点击对象的index
 
-      this._scrollTo(anchorIndex)
+      this._scrollTo(anchorIndex) // 滚动到指定位置
     },
+    // 右边滚动事件
     onShortcutTouchMove(e) {
-      let firstTouch = e.touches[0]
-      this.touch.y2 = firstTouch.pageY
-      let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0
-      let anchorIndex = this.touch.anchorIndex + delta
+      let firstTouch = e.touches[0] // 拿到点击对象
+      this.touch.y2 = firstTouch.pageY // 拿到点击对象的位置
+      let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0 // 计算滑动的标签数量
+      let anchorIndex = this.touch.anchorIndex + delta // 拿到当前的index位置
 
-      this._scrollTo(anchorIndex)
+      this._scrollTo(anchorIndex)// 滚动到指定位置
     },
-
+    // 由scroll组件发出，实时监控左边滚动的位置
     scroll(pos) {
       this.scrollY = pos.y
     },
-
+    // 滚动到指定位置
     _scrollTo(index) {
+      // 异常处理
       if (!index && index !== 0) {
         return
       }
+      // 边界处理
       if (index < 0) {
         index = 0
+      } else if (index > this.listHeight.length - 2) {
+        index = this.listHeight - 2
       }
+      // 刷新左边位置的记录，触发watch
+      this.scrollY = -this.listHeight[index]
+      // 滚动到指定位置
       this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
     },
 
+    // 得到左边每一个group的高度位置
     _calculateHeight() {
       this.listHeight = []
       const list = this.$refs.listGroup
@@ -121,29 +153,44 @@ export default {
     }
   },
   watch: {
+    // 数据变化，重新计算高度
     data() {
       this.$nextTick(() => {
         this._calculateHeight()
       })
     },
+    // 监控左边滚动的位置并设置默认Index和diff
+    // newY是负值
     scrollY(newY) {
       const listHeight = this.listHeight
+      // 边界处理，当滚动到顶部时，newY>0
       if (newY > 0) {
         this.currentIndex = 0
         return
       }
-
+      // 在中间部分滚动
       for (let i = 0; i < listHeight.length - 1; i++) {
-        let height1 = listHeight[1]
+        let height1 = listHeight[i]
         let height2 = listHeight[i + 1]
         if (-newY >= height1 && -newY < height2) {
           this.currentIndex = i
+          this.diff = height2 + newY
           return
         }
       }
       // 当滚动到底部，且-newY大于最后一个元素的上限
       this.currentIndex = listHeight.length - 2
-      console.log(this.currentIndex)
+    },
+    // 吸顶效果，当到顶部时，通过设置transform来产生效果
+    diff(newVal) {
+      // fixedTop:滑动到title部分时应滚动的值
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+      // 没滑动到每一个group的title时不改变dom
+      if (this.fixedTop === fixedTop) {
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
     }
   }
 }
@@ -204,6 +251,26 @@ export default {
         color: $color-theme;
       }
     }
+  }
+  .list-fixed {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    .fixed-title {
+      height: 30px;
+      line-height: 30px;
+      padding-left: 20px;
+      font-size: $font-size-small;
+      color: $color-text-l;
+      background: $color-highlight-background;
+    }
+  }
+  .loading-container {
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    transform: translateY(-50%);
   }
 }
 </style>
