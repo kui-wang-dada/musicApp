c6823812a443a3764a2b11f6dfee8f3e8ccca050
smallDada <872505550@qq.com>
2018-10-25 19:38:29 +0800

    浏览器兼容，歌手详情页特效

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -19,3 +19,35 @@ export function getData(el, name, val) {
   }
   return el.getAttribute(prefix + name)
 }
+// 创造一个元素
+let elementStyle = document.createElement('div').style
+// 拿到当前浏览器前缀
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+
+  return false
+})()
+
+export function prefixStyle(style) {
+  // 如果不是任何浏览器
+  if (vendor === false) {
+    return false
+  }
+  // 如果是默认情况
+  if (vendor === 'standard') {
+    return style
+  }
+  // 如果是其他浏览器
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,35 +1,51 @@
 <!--歌手详情页样式-->
 <template>
-    <div class=music-list>
-        <div class=back>
-            <i class='icon-back'></i>
-        </div>
-        <h1 class='title'
-            v-html='title'></h1>
-        <div class=bg-image
-             :style='bgStyle'
-             ref='bgImage'>
-            <div class=filter
-                 ref=filter></div>
-        </div>
-        <div class=bg-layer
-             ref=layer></div>
-        <scroll :data='songs'
-                @scroll='scroll'
-                :probe-type='probeType'
-                :listen-scroll='listenScroll'
-                class='list'
-                ref='list'>
-            <div class=song-list-wrapper>
-                <song-list :songs="songs"></song-list>
-            </div>
-        </scroll>
+  <div class=music-list>
+    <div class=back>
+      <i class='icon-back'></i>
     </div>
+    <h1 class='title'
+        v-html='title'></h1>
+    <div class=bg-image
+         :style='bgStyle'
+         ref='bgImage'>
+      <div class="play-wrapper">
+        <div ref="playBtn"
+             v-show="songs.length>0"
+             class="play">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class=filter
+           ref=filter></div>
+    </div>
+    <div class=bg-layer
+         ref=layer></div>
+    <scroll :data='songs'
+            @scroll='scroll'
+            :probe-type='probeType'
+            :listen-scroll='listenScroll'
+            class='list'
+            ref='list'>
+      <div class=song-list-wrapper>
+        <song-list :songs="songs"></song-list>
+      </div>
+      <div v-show="!songs.length"
+           class="loading-container">
+        <loading></loading>
+      </div>
+    </scroll>
+  </div>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import {prefixStyle} from 'common/js/dom.js' // 引入浏览器兼容写法
+
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 
 const RESERVED_HEIGHT = 40
 export default {
@@ -79,8 +95,35 @@ export default {
   watch: {
     scrollY(newY) {
       let translateY = Math.max(this.minTranslateY, newY)
-      this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
-      this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+      let zIndex = 0
+      // 下拉的时候放大背景图
+      let scale = 1
+      // 上移的时候模糊背景图
+      let blur = 0
+      const percent = Math.abs(newY / this.imageHeight)
+
+      if (newY > 0) {
+        zIndex = 10
+        scale = 1 + percent
+      } else {
+        blur = Math.min(20 * percent, 20)
+      }
+
+      if (newY < this.minTranslateY) {
+        zIndex = 10
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
+      } else {
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
+      }
+
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
+      this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
+      this.$refs.bgImage.style[backdrop] = `blur(${blur})px`
     }
   }
 }
@@ -128,6 +171,34 @@ export default {
     padding-top: 70%;
     transform-origin: top;
     background-size: cover;
+    .play-wrapper {
+      position: absolute;
+      bottom: 20px;
+      z-index: 50;
+      width: 100%;
+      .play {
+        box-sizing: border-box;
+        width: 135px;
+        padding: 7px 0;
+        margin: 0 auto;
+        text-align: center;
+        border: 1px solid $color-theme;
+        color: $color-theme;
+        border-radius: 100px;
+        font-size: 0;
+        .icon-play {
+          display: inline-block;
+          vertical-align: middle;
+          margin-right: 6px;
+          font-size: $font-size-medium-x;
+        }
+        .text {
+          display: inline-block;
+          vertical-align: middle;
+          font-size: $font-size-small;
+        }
+      }
+    }
     .filter {
       position: absolute;
       top: 0;
@@ -150,6 +221,12 @@ export default {
     background: $color-background;
     .song-list-wrapper {
       padding: 20px 30px;
+    }
+    .loading-container {
+      position: absolute;
+      width: 100%;
+      top: 50%;
+      transform: translateY(-50%);
     }
   }
 }
